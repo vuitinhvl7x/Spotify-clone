5c3b9051f574389714f435d9952b1dfe5ff17295
Xuan_Hai <xuanhai2708@gmail.com>
2024-02-23 05:04:11 +0300

    add login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
+import Login from "./components/Login";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Login></Login>
+    </div>
+  );
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Login() {
+  const handleClick = async () => {};
+  return (
+    <Container>
+      <img
+        src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_Black.png"
+        alt="spotify"
+      />
+      <button onClick={handleClick}>Connect Spotify</button>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+  width: 100vw;
+  background-color: #1db954;
+  gap: 5rem;
+  img {
+    height: 20vh;
+  }
+  button {
+    padding: 1rem 5rem;
+    border-radius: 5rem;
+    background-color: black;
+    color: #49f585;
+    border: none;
+    font-size: 1.4rem;
+    cursor: pointer;
+  }
+`;

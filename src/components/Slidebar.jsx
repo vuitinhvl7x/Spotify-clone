a2f5ef93966d08fcaf81213d992fa5b6fd954198
Xuan_Hai <xuanhai2708@gmail.com>
2024-02-23 16:35:03 +0300

    create spotify layout

---
+++ src/components/Slidebar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Slidebar() {
+  return <Container>Slidebar</Container>;
+}
+
+const Container = styled.div`
+  background-color: black;
+`;

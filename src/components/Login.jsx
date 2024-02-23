5b310d01ffef53470cfd83d28962c872e41d54ee
Xuan_Hai <xuanhai2708@gmail.com>
2024-02-23 05:41:00 +0300

    get access token from Spotify

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,23 @@ import React from "react";
 import styled from "styled-components";
 
 export default function Login() {
-  const handleClick = async () => {};
+  const handleClick = async () => {
+    const client_id = "647fe8b88c7a4facbccbf8a3e3e9b455";
+    const redirect_uri = "http://localhost:3000/";
+    const api_uri = "https://accounts.spotify.com/authorize";
+    const scope = [
+      "user-read-private",
+      "user-read-email",
+      "user-modify-playback-state",
+      "user-read-playback-state",
+      "user-read-currently-playing",
+      "user-read-recently-played",
+      "user-top-read",
+    ];
+    window.location.href = `${api_uri}?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope.join(
+      " "
+    )}&response_type=token&show_dialog=true`;
+  };
   return (
     <Container>
       <img
